a8d69d7426f3d931d5ac1c25e6a3b5b427463ee6
p1eXu5 <v1adp1exu5@gmail.com>
2022-05-23 20:06:20 +0300

    [ test ] - add MapToITests, mock logger  factory

---
+++ test/p1eXu5.AutoProfile.Tests/IntegrationTests/MapToITests.Types.cs
@@ -0,0 +1,33 @@
+﻿using p1eXu5.AutoProfile.Attributes;
+
+namespace p1eXu5.AutoProfile.Tests.IntegrationTests;
+
+public partial class MapToITests
+{
+    public enum TestEnum
+    {
+        One,
+        Two,
+        Three
+    }
+
+    [MapTo(typeof(SlaveSimpleClassModel))]
+    public class MasterSimpleClassModel
+    {
+        public int IntProp { get; init; }
+        public string StringProp { get; init; }
+        public bool BoolProp { get; init; }
+        public double DoubleProp { get; init; }
+        public TestEnum EnumProp { get; init; }
+    }
+
+    public class SlaveSimpleClassModel
+    {
+        public int IntProp { get; init; }
+        public string StringProp { get; init; }
+        public bool BoolProp { get; init; }
+        public double DoubleProp { get; init; }
+        public TestEnum EnumProp { get; init; }
+    }
+}
+    
---
+++ test/p1eXu5.AutoProfile.Tests/Usings.cs
@@ -0,0 +1,4 @@
+﻿global using NUnit.Framework;
+global using AutoMapper;
+global using p1eXu5.AutoProfile;
+global using p1eXu5.AutoProfile.Attributes;
